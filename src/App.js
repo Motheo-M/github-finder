bc83429efcab19669414048a718b0cdbce36dcb3
Motheo <motheotb@gmail.com>
2023-03-13 12:20:57 +0200

    change to files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import{ BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+
+        <main>Content</main>
+      </div>
+    </Router>
   );
 }
 
